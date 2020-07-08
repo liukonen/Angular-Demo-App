b45f048a5fc391a7456d60c22941ce9cb235489c
Luke Liukonen <liukonen@gmail.com>
2020-07-08 14:11:31 -0500

    Display the List

--- angular-tour-of-heroes/src/app/heros/heros.component.ts
+++ angular-tour-of-heroes/src/app/heros/heros.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import {Hero} from './hero';
+import {HEROES} from './mockHeros';
 
 @Component({
   selector: 'app-heros',
@@ -9,7 +9,17 @@ import {Hero} from './hero';
 })
 export class HerosComponent implements OnInit {
   hero: Hero = {id: 1, name: "Superman"}
+  heroes = HEROES;
+  
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
+  
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
